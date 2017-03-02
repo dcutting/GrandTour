8b439f374411c271d2efebd532656f5cfc7eda89
Dan Cutting <dan@cutting.io>
2017-03-02 21:45:27 +0000

    Put London on the map.

--- GrandTour/ViewController.swift
+++ GrandTour/ViewController.swift
@@ -1,6 +1,24 @@
 //  Copyright © 2017 Dan Cutting. All rights reserved.
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var annotations: [MKAnnotation] = []
+
+    override func viewDidLoad() {
+        let location = CLLocationCoordinate2DMake(51.5, -0.1)
+        let annotation = makeAnnotation(name: "London", coordinate: location)
+        mapView.addAnnotation(annotation)
+    }
+    
+    func makeAnnotation(name: String, coordinate: CLLocationCoordinate2D) -> MKAnnotation {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = name
+        return annotation
+    }
 }
