ebe1bdcd665fcfb66c4deab8e8ca9f7ff1089df3
Dan Cutting <dan@cutting.io>
2017-03-02 22:07:46 +0000

    Load annotation data from JSON file and put it on the map

--- GrandTour/ViewController.swift
+++ GrandTour/ViewController.swift
@@ -7,12 +7,12 @@ class MapViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
     
-    var annotations: [MKAnnotation] = []
-
     override func viewDidLoad() {
-        let location = CLLocationCoordinate2DMake(51.5, -0.1)
-        let annotation = makeAnnotation(name: "London", coordinate: location)
-        mapView.addAnnotation(annotation)
+        loadAnnotations { annotations in
+            for annotation in annotations {
+                self.mapView.addAnnotation(annotation)
+            }
+        }
     }
     
     func makeAnnotation(name: String, coordinate: CLLocationCoordinate2D) -> MKAnnotation {
@@ -20,5 +20,54 @@ class MapViewController: UIViewController {
         annotation.coordinate = coordinate
         annotation.title = name
         return annotation
+    }
+    
+    func loadAnnotations(completion: @escaping ([MKAnnotation]) -> Void) {
+        guard let url = Bundle.main.url(forResource: "landmarks", withExtension: "json") else {
+            completion([])
+            return
+        }
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                completion([])
+                return
+            }
+            do {
+                guard let jsonLandmarks = try JSONSerialization.jsonObject(with: data) as? [Any] else {
+                    completion([])
+                    return
+                }
+                var annotations: [MKAnnotation] = []
+                for jsonLandmark in jsonLandmarks {
+                    guard let jsonLandmark = jsonLandmark as? [String: Any] else {
+                        completion([])
+                        return
+                    }
+                    guard let name = jsonLandmark["name"] as? String else {
+                        completion([])
+                        return
+                    }
+                    guard let jsonCoordinate = jsonLandmark["coordinate"] as? [String: Double] else {
+                        completion([])
+                        return
+                    }
+                    guard let latitude = jsonCoordinate["latitude"] else {
+                        completion([])
+                        return
+                    }
+                    guard let longitude = jsonCoordinate["longitude"] else {
+                        completion([])
+                        return
+                    }
+                    let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+                    let annotation = self.makeAnnotation(name: name, coordinate: coordinate)
+                    annotations.append(annotation)
+                }
+                completion(annotations)
+            } catch {
+                completion([])
+            }
+        }
+        task.resume()
     }
 }
