8a44aab4e5a50df773e861581bcd6acea422e917
Dan Cutting <dan@cutting.io>
2017-03-19 22:10:22 +0000

    Enable Done button when valid for MVC

--- GrandTour/LandmarkCreatorViewController.swift
+++ GrandTour/LandmarkCreatorViewController.swift
@@ -9,13 +9,33 @@ protocol LandmarkCreatorViewControllerDelegate: class {
 class LandmarkCreatorViewController: UIViewController {
     
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var doneButton: UIButton!
     
     weak var delegate: LandmarkCreatorViewControllerDelegate?
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameTextField.addTarget(self, action: #selector(textChanged), for: .editingChanged)
+        updateDoneButton()
+    }
+    
     @IBAction func tappedDone(_ sender: Any) {
         guard let name = nameTextField.text else { return }
-        guard name.characters.count > 2 else { return }
+        guard isValid() else { return }
         dismiss(animated: true)
         delegate?.createdLocation(named: name)
+    }
+
+    @objc private func textChanged() {
+        updateDoneButton()
+    }
+    
+    private func updateDoneButton() {
+        doneButton.isEnabled = isValid()
+    }
+    
+    private func isValid() -> Bool {
+        guard let text = nameTextField.text else { return false }
+        return text.characters.count > 2
     }
 }
